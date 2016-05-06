1910d0ed61e5f593841fcd02ca6fa09073e79b86
Daniel Bachhuber <daniel@handbuilt.co>
2016-05-06 09:53:19 -0700

    Use `permission_callback` to verify the request can use the endpoint
    If the request / user cannot use the endpoint, return a `WP_Error`
    object (with `status=>{code}` to set the HTTP code) so that the server
    returns an error.

--- rest-api-demo.php
+++ rest-api-demo.php
@@ -24,11 +24,20 @@ add_action( 'rest_api_init', function() {
 
 	register_rest_route( 'rad/v1', 'site-info', array(
 		array(
-			'methods'            => 'GET',
-			'callback'           => function( $request ) {
+			'methods'             => 'GET',
+			'callback'            => function( $request ) {
 				return array(
 					'phone_number' => get_option( 'phone_number' ),
 				);
+			},
+			'permission_callback' => function() {
+				if ( is_user_logged_in() ) {
+					return true;
+				}
+				return new WP_Error(
+					'rad_unauthorized',
+					'You are not authorized to view this resource.',
+					array( 'status' => 401 ) );
 			},
 		),
 		array(
@@ -38,6 +47,15 @@ add_action( 'rest_api_init', function() {
 				return array(
 					'phone_number' => get_option( 'phone_number' ),
 				);
+			},
+			'permission_callback' => function() {
+				if ( current_user_can( 'manage_options' ) ) {
+					return true;
+				}
+				return new WP_Error(
+					'rad_unauthorized',
+					'You are not authorized to update this resource.',
+					array( 'status' => is_user_logged_in() ? 403 : 401 ) );
 			},
 		),
 	) );
